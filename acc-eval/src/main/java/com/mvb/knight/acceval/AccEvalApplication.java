d2d0ec61f3070c1391f751f00455cf2e60aad0a0
zhangpeng57 <zhangpeng57@58.com>
2023-03-13 16:18:07 +0800

    first commit

---
+++ acc-eval/src/main/java/com/mvb/knight/acceval/AccEvalApplication.java
@@ -0,0 +1,13 @@
+package com.mvb.knight.acceval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccEvalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccEvalApplication.class, args);
+	}
+
+}
---
+++ acc-eval/src/test/java/com/mvb/knight/acceval/AccEvalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvb.knight.acceval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccEvalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
